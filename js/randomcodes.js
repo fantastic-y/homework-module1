ee178546488fd4a3dbb164557278dbddac8258d7
Fan Yang <fantasticy@Fans-MacBook-Pro.local>
2021-07-19 20:42:48 -0700

    updated the randomcode

---
+++ js/randomcodes.js
@@ -0,0 +1,25 @@
+/* ---- random codes ---- */
+//function to generate combination of characters
+function generateCode() {
+    //create variables to store generated codes and the type of characters we want to show as codes
+    var code = ' '; //initialize to null value
+    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+
+    //generate character multiple times using a loop
+    for (i = 1; i <=8; i++) {
+        var char = Math.random()* str.length; //random select a character from the variable and then store in a new variable
+        code += str.charAt(char) //accumulate the generated character into a string of 8 characters
+    }
+    return code; //return the final accumulated string when loop ends
+}
+
+//get html element to display
+document.getElementById("codes").innerHTML = generateCode();
+
+//disable button
+function disableButton() {
+    document.getElementById("submit").disabled = true;
+}
+
+//activate function
+disableButton();

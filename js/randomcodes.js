7c2592e27c9ee4bdc996c55b170ecf845aac4ddd
Fan Yang <fantasticy@Fans-MacBook-Pro.local>
2021-07-21 18:44:07 -0700

    updated the partner section

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -3,6 +3,9 @@
 function generateCode() {
     //create variables to store generated codes and the type of characters we want to show as codes
     var code = ' '; //initialize to null value
+    var getCode = ' '; //to store entered code
+    var btnvalue; //for button boolean value
+    //create variable to hold the type of characters we want to show as codes
     var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
 
     //generate character multiple times using a loop
@@ -17,8 +20,32 @@ function generateCode() {
 document.getElementById("codes").innerHTML = generateCode();
 
 //disable button
-function disableButton() {
-    document.getElementById("submit").disabled = true;
+function disableButton(btnvalue) {
+    document.getElementById("submit").disabled = btnvalue; //able/disable button
+    if (btnvalue == true) {
+        //set button and label color translucent
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 0.3)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+    } else {
+        //set button and label color with no transparency
+        document.getElementById("submit").style.backgroundColor = "rgba(73, 119, 209, 1)";
+        document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+    }
+}
+
+//listen to user input code
+var codebox = document.getElementById("codeentered"); //get textbox
+codebox.addEventListener("input", evaluateCode); //listen to code entered in textbox
+
+//run function if detected user had entered a character in textbox
+function evaluateCode() {
+    getCode = document.getElementById("codeentered").value; //get character entered
+    var charset1 = getCode.trim(); //remove any hidden characters entered
+    var charset2 = code.trim(); //remove any hidden characters generated
+    //test if code entered matches the number of genereated characters
+    if (charset1.length == charset2.length && charset1 == charset2) {
+        disableButton(false); //if match, run the function to enable button
+    }
 }
 
 //activate function
